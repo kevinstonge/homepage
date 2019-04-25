5880de3079d2246998eefc476540d061a57c4e66
Kevin St.Onge <kevinstonge@gmail.com>
2019-04-24 20:32:26 -0400

    refined header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
 import Logo from '../components/Logo';
-
+//placeholder function; should be a react functional component I think:
+function navigateHome() { console.log('hi'); }
 export class Header extends Component {
   render() {
     return (
         <div>
-            <header className="App-header">
-                <div id="header-logo">
+            <header className="App-header visual-container" onClick={navigateHome}>
+                <div id="header-left" className="header-block">
                     <Logo />
                 </div>
-                <h1>The Website of Kevin St.Onge</h1>
+                <div id="header-middle" className="header-block">
+                  <h1>The Website of Kevin St.Onge</h1>
+                </div>
+                <div id="header-right" className="header-block">
+                </div>
             </header>
         </div>
     )
