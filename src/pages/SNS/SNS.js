3214ea978309ba60ea23d7845b4e00180f10e9c9
Kevin St.Onge <kevinstonge@gmail.com>
2019-04-27 11:41:06 -0400

    SNS.js being used for experimenting

--- src/pages/SNS/SNS.js
+++ src/pages/SNS/SNS.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
 
 export default class SNS extends Component {
+  constructor(props) {  
+    super(props);
+    this.state = {
+      x:0,
+    };
+    this.timeoutVar = setTimeout(this.functions.fu);
+    this.functions = {
+      fu: (n) => { n++; this.setState({x:n}); console.log(n); setTimeout(this.functions.fu,1000,n); }
+    }
+  }
   render() {
+
     return (
-      <div>Speak and Spell!</div>
+      <div>
+      <div id="sns">Speak and Spell!</div>
+      <div id="x">{this.state.x}</div>
+      </div>
     )
+  }
+  componentDidMount() {
+    setTimeout(this.functions.fu,1000,this.state.x)
+  }
+  componentWillUnmount() {
+    clearTimeout(this.functions.fu);
   }
 }
