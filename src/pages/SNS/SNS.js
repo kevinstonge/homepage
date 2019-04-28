f8b498f7eb701aa0f3ff07068e93f7b5cd62e338
Kevin St.Onge <kevinstonge@gmail.com>
2019-04-28 19:13:33 -0400

    creates SNSfunc.js

--- src/pages/SNS/SNS.js
+++ src/pages/SNS/SNS.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import SNSsvg from './SNSsvg';
 import './SNS.css';
+import {test, test2} from './SNSfunc';
 export default class SNS extends Component {
   constructor(props) {  
     super(props);
@@ -19,12 +20,14 @@ export default class SNS extends Component {
         <div id="x">{this.state.x}</div>
         <div id="SNScontainer">
         <SNSsvg SNSButtonPress={this.SNSButtonPress}/>
+        
         </div>
       </div>
     )
   }
   componentDidMount() {
     this.timeoutVar = setTimeout(this.functions.fu,1000,this.state.x)
+    {test2(5)}
   }
   componentWillUnmount() {
     clearTimeout(this.timeoutVar);
---
+++ src/pages/SNS/SNSfunc.js
@@ -0,0 +1,8 @@
+function test (x) {
+    console.log(x);
+}
+function test2 (y) {
+    console.log(y);
+}
+
+export {test, test2};
