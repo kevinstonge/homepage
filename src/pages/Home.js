67294bb65f23665e188f98b40a9c27438e7dc1a9
Kevin St.Onge <kevinstonge@gmail.com>
2019-05-04 17:59:38 -0400

    added some text to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
   render() {
     return (
       <p>
-        hello from Home.js
+        I'm a freelance web developer. This website is a demonstration of my abilities to work in HTML, CSS, Javascript (ES6), and ReactJS. You can view all of the source code in <a href="https://github.com/kevinstonge/homepage">my github repository</a>
       </p>
     )
   }
